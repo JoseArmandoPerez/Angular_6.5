ed6ac6369fe98a1c99bf98145684e5995a0e0c39
JoseArmandoPerez <118926810+JoseArmandoPerez@users.noreply.github.com>
2023-03-29 17:42:25 -0600

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { GohanComponent } from './gohan/gohan.component';
+import { GokuComponent } from './goku/goku.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, GohanComponent,GokuComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
